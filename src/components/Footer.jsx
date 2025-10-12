483113e517bd2e45cec21aa7898ad3143734acb5
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-12 21:04:20 +0200

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-gray-200 text-center p-4 mt-4">
+    &copy; {new Date().getFullYear()} Travel Planner. All rights reserved.
+  </footer>
+);
+
+export default Footer;
