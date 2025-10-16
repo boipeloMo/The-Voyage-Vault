011aca63aca67f2924c45410f21d4cfd351037a5
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-16 11:21:10 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
-content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
+```js
+module.exports = {
+content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+theme: {
+extend: {},
+},
+plugins: [],
+}
