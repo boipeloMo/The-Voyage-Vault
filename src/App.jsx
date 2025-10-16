05d08f8c2f52292800f94673c2f1fd7aa271c5b0
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-16 11:26:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Destinations from './pages/Destinations';
-import Itinerary from './pages/Itinerary';
+import React, { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Destinations from './pages/Destinations'
+import ItineraryPage from './pages/ItineraryPage'
 
-function App() {
-  return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1 p-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/destinations" element={<Destinations />} />
-            <Route path="/itinerary" element={<Itinerary />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+
+export default function App(){
+// top-level state for chosen itinerary items
+const [itinerary, setItinerary] = useState([])
+
+
+const addToItinerary = (destination) => {
+setItinerary(prev => [...prev, destination])
 }
 
-export default App;
+
+const removeFromItinerary = (index) => {
+setItinerary(prev => prev.filter((_, i) => i !== index))
+}
+
+
+return (
+<div className="flex flex-col min-h-screen">
+<Header />
+<main className="flex-1 py-8 bg-slate-50">
+<div className="container">
+<Routes>
+<Route path="/" element={<Home addToItinerary={addToItinerary} />} />
+<Route path="/destinations" element={<Destinations addToItinerary={addToItinerary} />} />
+<Route path="/itinerary" element={<ItineraryPage itinerary={itinerary} removeFromItinerary={removeFromItinerary} />} />
+</Routes>
+</div>
+</main>
+<Footer />
+</div>
+)
+}
