6230b2f4804f1f7e91f79489e6b9b8b9d39da64c
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-23 23:12:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Destinations from "./pages/Destinations";
 import Itinerary from "./pages/Itinerary";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
@@ -15,9 +17,12 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/destinations" element={<Destinations />} />
           <Route path="/itinerary" element={<Itinerary />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
       <Footer />
     </div>
   );
 }
+
