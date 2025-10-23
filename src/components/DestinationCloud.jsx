c5c7eddc74608bad354f1b74f8e76c75bfc91750
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-23 22:50:30 +0200

    Create DestinationCloud.jsx

---
+++ src/components/DestinationCloud.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DestinationCard from "./DestinationCard";
+
+export default function DestinationCloud({ items = [], onAdd }) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      {items.map(item => (
+        <DestinationCard key={item.id} dest={item} onAdd={onAdd} />
+      ))}
+    </div>
+  );
+}
