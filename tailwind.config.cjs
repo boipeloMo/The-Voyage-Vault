0f1b7c496986f3ba25d99ec3e7d1c887243acd88
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-24 10:39:52 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        vaultBlue: '#0f172a',
-        accent: '#0ea5a4'
-      }
-    }
-  },
-  plugins: [],
+content: [
+'./index.html',
+'./src/**/*.{js,jsx,ts,tsx}',
+],
+theme: {
+extend: {
+colors: {
+vaultBlue: '#0f172a',
+accent: '#0ea5a4'
+}
+}
+},
+plugins: [],
 }
